348e948105f60acb20d59bde7eb953caab5e2297
Arun S <starunselvam@gmail.com>
2023-07-01 23:29:31 -0700

    main

--- delete.py
+++ delete.py
@@ -4,6 +4,10 @@ def delete(sno):
     for company in data["Business"]:
         if str (company["sno"])==str(sno):
             data["Business"].remove(company)
+    i=1
+    for company in data["Business"]:
+        company["sno"]=str(i)
+        i+=1
     json_utils.write_json("data/business.json",data)
          
     
